de2aaa39ae5a1215788586d80b1d0902f6978480
xnd2000 <97997012+xnd2000@users.noreply.github.com>
2022-04-29 14:38:22 -0400

    ex7

--- exercise-07/script.js
+++ exercise-07/script.js
@@ -1,7 +1,7 @@
 let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 let adjective = ['hastily', 'quickly', 'deliciously', 'blissfully', 'happily', 'pensively'];
-let noun = ['a piece of toast', 'a cracker', 'a chocolate bar', 'a salad', 'avocado toast', 'eggs', 'ramen', 'sushi', 'melons', 'rice', 'spinach', 'cheese', 'carrots'];
-let verb = ['run', 'walk', 'read', 'dance', 'call a friend', 'go to a museum', 'draw', 'write a song']
+let noun = ['a piece of toast', 'a cracker', 'a chocolate bar', 'a pizza', 'avocado toast', 'eggs', 'ramen', 'sushi', 'melons', 'rice', 'spinach', 'cheese', 'carrots'];
+let verb = ['ran', 'walked', 'read', 'danced', 'called a friend', 'went to a museum', 'drew', 'wrote a song']
 
 function renderPoem() {
     const RandomMonth = Math.floor(Math.random() * month.length);
@@ -9,7 +9,7 @@ function renderPoem() {
     const RandomNoun = Math.floor(Math.random() * noun.length);
     const RandomVerb = Math.floor(Math.random() * verb.length);
     
-    document.getElementById("poem").innerText = `On a random day in ${month[RandomMonth]}, I ate ${noun[RandomNoun]} rather ${adjective[RandomAdjective]} and proceeded to ${verb[RandomVerb]}.`
+    document.getElementById("poem").innerText = `On a sunny day in ${month[RandomMonth]}, I ingested ${noun[RandomNoun]} very ${adjective[RandomAdjective]} and then ${verb[RandomVerb]}.`
 }
 
 renderPoem()
